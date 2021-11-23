5d78a564a1507dcd13b2a28284685529b1b7fcbe
Noah <bitterhippo@gmail.com>
2021-11-23 11:53:21 +0900

    asteroid logic

---
+++ Asteroid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+  [SerializeField]
+  private float _rotateSpeed = 19.0f;
+
+  [SerializeField]
+  private GameObject _player;
+  [SerializeField]
+  private GameObject _explosion;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+  }
+
+  private void OnTriggerEnter2D(Collider2d other)
+  {
+    Player player = other.transform.GetComponent<Player>();
+
+    if (other.tag == player)
+    {
+      Vector3 currentLocation = new Vector3(0, 0, 0);
+      GameObject newExplion = Instantiate(_explosion, currentLocation, Quaternion.identity);
+      Destory(this.gameObject);
+    }
+  }
+}
--- Enemy.cs
+++ Enemy.cs
@@ -4,61 +4,72 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    private float _movementSpeed = 4.0f;
+  [SerializeField]
+  private float _movementSpeed = 4.0f;
 
-    private Player _player;
-    // Start is called before the first frame update
-    void Start()
+  private Player _player;
+  private Animator _anim;
+  // Start is called before the first frame update
+  void Start()
+  {
+    _player = GameObject.Find("Player").GetComponent<Player>();
+
+    if (_player == null)
     {
-        _player = GameObject.Find("Player").GetComponent<Player>();
-
-        if (_player == null)
-        {
-           Debug.LogError("@Enemy.cs > _player not found");
-        }
+      Debug.LogError("@Enemy.cs > _player = null");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Movement();
+    _anim = GetComponent<Animator>();
 
-        if (transform.position.y < -6.0f)
-        {
-            float randomX = Random.Range(-8.0f, 8.0f);
-            transform.position = new Vector3(randomX, 7, 0);
-        }
+    if (_anim == null)
+    {
+      Debug.LogError("@Enemy.cs > _anim = null");
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Movement();
+
+    if (transform.position.y < -6.0f)
+    {
+      float randomX = Random.Range(-8.0f, 8.0f);
+      transform.position = new Vector3(randomX, 7, 0);
+    }
+  }
+
+  void Movement()
+  {
+    transform.Translate(Vector3.down * _movementSpeed * Time.deltaTime);
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    Player player = other.transform.GetComponent<Player>();
+
+    if (other.tag == "Player")
+    {
+
+      if (player != null)
+      {
+        player.Damage();
+      }
+      _anim.SetTrigger("OnEnemyDeath");
+      _movementSpeed = 0;
+      Destroy(this.gameObject, 2.8f);
     }
 
-    void Movement() 
+    if (other.tag == "Lazer")
     {
-        transform.Translate(Vector3.down * _movementSpeed * Time.deltaTime);
+      Destroy(other.gameObject);
+      if (_player != null)
+      {
+        _player.UpdateScore(10);
+      }
+      _anim.SetTrigger("OnEnemyDeath");
+      _movementSpeed = 0;
+      Destroy(this.gameObject, 2.8f);
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Player player = other.transform.GetComponent<Player>();
-
-        if (other.tag == "Player") 
-        {
-            
-            if (player != null)
-            {
-                player.Damage();
-            }
-
-            Destroy(this.gameObject);
-        }
-
-        if (other.tag == "Lazer")
-        {
-            Destroy(other.gameObject);
-            if (_player != null)
-            {
-                _player.UpdateScore(10);
-            }
-            Destroy(this.gameObject);
-        }
-    }
+  }
 }
